2d3f423e4f968b7869097ebceec8542081a7fa32
Barny <ismaquesada23@icloud.com>
2025-03-09 16:04:37 -0600

    Inicio Logica login (empezando)

---
+++ Capa_Presentacion_Tienda/Controllers/autenticadorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Capa_Presentacion_Admin.Controllers
+{
+    public class autenticadorController : Controller
+    {
+        // GET: autenticador
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
