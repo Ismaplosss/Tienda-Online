5f8edcdff61ae45c9623c8ed48a60d55468849b3
Barny <ismaquesada23@icloud.com>
2025-03-12 16:25:25 -0600

    Actualizacion de master

--- Capa_Presentacion_Tienda/App_Start/RouteConfig.cs
+++ Capa_Presentacion_Tienda/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Capa_Presentacion_Tienda
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "autenticador", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
