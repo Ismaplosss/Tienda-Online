cbb5ae7062ac0362b680e74ae37cda610b3d371d
Barny <ismaquesada23@icloud.com>
2025-03-21 10:18:35 -0600

    actualizacion de dependancias (error de la master

--- CapaPresentasion_Admin/App_Start/BundleConfig.cs
+++ CapaPresentasion_Admin/App_Start/BundleConfig.cs
@@ -20,7 +20,10 @@ namespace CapaPresentasion_Admin
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new Bundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap.js"
+                      
+                      ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
